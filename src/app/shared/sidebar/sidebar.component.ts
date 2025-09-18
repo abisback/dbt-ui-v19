5fe25e2888be21caf4e0183c68a60a5a87ed985a
ujjwalkant.nic@gmail.com <ujjwalkant.nic@gmail.com>
2025-09-18 17:11:19 +0530

    Fix - Sidebar hide after navigate to new url

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 import { USERROLE } from '../../../app_enum';
 import { share } from 'rxjs';
 import { SharedModule } from '../shared.module';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   imports: [SharedModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
-    IsNotOperator:boolean = true;
-  IsStateLevel:boolean = false;
-  IsSuperAdmin:boolean = false;
-  IsNodal:boolean=false;
-  IsStateNodal:boolean=false;
-  IsDepartmentAdmin:boolean=false;
+  IsNotOperator: boolean = true;
+  IsStateLevel: boolean = false;
+  IsSuperAdmin: boolean = false;
+  IsNodal: boolean = false;
+  IsStateNodal: boolean = false;
+  IsDepartmentAdmin: boolean = false;
 
   //-------------------
- // @ViewChild('sidenav') sidenav: MatSidenav;
+  // @ViewChild('sidenav') sidenav: MatSidenav;
   isExpanded = true;
   showSubmenu: boolean = false;
   isShowing = false;
@@ -27,38 +27,35 @@ export class SidebarComponent {
 
   //--------------------
 
-  constructor() { }
+  @Output() toggleSideBar = new EventEmitter<void>();
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    let userRole = localStorage.getItem("role");
+    let userRole = localStorage.getItem('role');
     // console.log(userRole);
 
-    if(userRole == USERROLE['Department Operator']){
+    if (userRole == USERROLE['Department Operator']) {
       this.IsNotOperator = false;
     }
-    if(userRole == USERROLE['Department Admin']){
+    if (userRole == USERROLE['Department Admin']) {
       this.IsDepartmentAdmin = true;
     }
 
-    if((userRole == USERROLE['Super Admin']))
-    {
+    if (userRole == USERROLE['Super Admin']) {
       this.IsSuperAdmin = true;
     }
 
-    if((userRole == USERROLE['State Level Admin']))
-    {
+    if (userRole == USERROLE['State Level Admin']) {
       this.IsStateLevel = true;
     }
-    if((userRole == USERROLE['Department Nodal']))
-    {
+    if (userRole == USERROLE['Department Nodal']) {
       this.IsNodal = true;
     }
-    if((userRole == USERROLE['State Nodal']))
-    {
+    if (userRole == USERROLE['State Nodal']) {
       this.IsStateNodal = true;
     }
   }
-//-----------------------
+  //-----------------------
   // mouseenter() {
   //   if (!this.isExpanded) {
   //     this.isShowing = true;
@@ -72,6 +69,12 @@ export class SidebarComponent {
   // }
 
   //-----------------------
-
-
+  ngAfterViewInit() {
+    // Listen to route changes
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.toggleSideBar.emit();
+      }
+    });
+  }
 }
